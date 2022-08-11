36cea3b629d1c35b97321f2bea4005cfd79b1f48
Cody Ebberson <cody@ebberson.com>
2022-08-11 13:58:04 -0700

    Added source-map-explorer

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     port: 3000,
   },
