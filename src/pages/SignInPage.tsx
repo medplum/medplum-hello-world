12b8342d660a78f1f9f448cfd7aa6ff0eba4e51f
Rahul Agarwal <rahul@medplum.com>
2022-11-08 01:36:43 -0800

    Added default google client id

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -5,7 +5,10 @@ import { useNavigate } from 'react-router-dom';
 export function SignInPage(): JSX.Element {
   const navigate = useNavigate();
   return (
-    <SignInForm onSuccess={() => navigate('/')}>
+    <SignInForm
+      googleClientId="921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com"
+      onSuccess={() => navigate('/')}
+    >
       <Logo size={32} />
       <h1>Sign in to Medplum</h1>
     </SignInForm>
