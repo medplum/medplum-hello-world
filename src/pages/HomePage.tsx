c29965c8e8d37335e838ccc7ec5e6bb8d695cfb5
Rahul Agarwal <rahul@medplum.com>
2023-01-16 14:45:37 -0800

    Remove "thrown promises" in favor of standard React patterns

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,34 @@
-import { Loader } from '@mantine/core';
+import { Anchor, Loader } from '@mantine/core';
 import { formatGivenName } from '@medplum/core';
 import { HumanName, Patient, Practitioner } from '@medplum/fhirtypes';
 import { Document, ResourceBadge, useMedplum, useMedplumProfile } from '@medplum/react';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+/**
+ * Home page that greets the user and displays a list of patients
+ * @returns
+ */
 export function HomePage(): JSX.Element {
+  // Call useMedplum() to get the current MedplumClient instance
+  // https://www.medplum.com/docs/sdk/classes/MedplumClient
   const medplum = useMedplum();
+  // useMedplumProfile() returns the "profile resource" associated with the user.
+  // This can be a Practitioner, Patient, or RelatedPerson depending on the user's role in the project.
+  // See the "Register" tutorial for more detail
+  // https://www.medplum.com/docs/tutorials/register
   const profile = useMedplumProfile() as Practitioner;
-  const patients: Patient[] = medplum.searchResources('Patient', '_sort=-_lastUpdated').read();
 
-  if (!patients) {
-    return <Loader />;
-  }
+  // Conduct a simple FHIR search for all Patient resources, sorted by last updated time (descending)
+  const [patients, setPatients] = useState<Patient[]>([]);
+  useEffect(() => {
+    medplum.searchResources('Patient', '_sort=-_lastUpdated').then(setPatients);
+  }, [medplum]);
+
   return (
     <Document>
-      <h1>Welcome {formatGivenName(profile.name?.[0] as HumanName)}</h1>
+      <h1>
+        Welcome &nbsp;
+        <Anchor href="/profile">{formatGivenName(profile.name?.[0] as HumanName)}</Anchor>
+      </h1>
       <h3>Patients</h3>
       {patients.map((e) => (
         <div key={e.id}>
--- src/pages/ResourcePage.tsx
+++ src/pages/ResourcePage.tsx
@@ -1,6 +1,6 @@
 import { getDisplayString } from '@medplum/core';
 import { Patient, ResourceType } from '@medplum/fhirtypes';
-import { Document, ResourceTable, useMedplum } from '@medplum/react';
+import { Document, ResourceTable, useMedplum, useResource } from '@medplum/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { PatientHeader } from './PatientHeader';
@@ -10,9 +10,11 @@ import { PatientHeader } from './PatientHeader';
  * It uses the Medplum `<ResourceTable>` component to display a resource.
  */
 export function ResourcePage(): JSX.Element | null {
-  const medplum = useMedplum();
   const { resourceType, id } = useParams();
-  const resource = medplum.readResource(resourceType as ResourceType, id as string).read();
+  const resource = useResource({ resourceType: resourceType as ResourceType, id });
+  if (!resource) {
+    return null;
+  }
   return (
     <>
       {resourceType === 'Patient' && <PatientHeader patient={resource as Patient} />}
