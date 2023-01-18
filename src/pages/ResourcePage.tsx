8947768c2989a399da922cdc2ac2c3f47c55f334
rahul1 <rahul@medplum.com>
2023-01-17 22:00:14 -0800

    Fix links to non-existent pages (#25)
    Fix links to non-existent pages

--- src/pages/ResourcePage.tsx
+++ src/pages/ResourcePage.tsx
@@ -1,27 +1,39 @@
 import { getDisplayString } from '@medplum/core';
-import { Patient } from '@medplum/fhirtypes';
-import { Document, ResourceTable, useResource } from '@medplum/react';
-import React from 'react';
+import { Resource, ResourceType } from '@medplum/fhirtypes';
+import { Document, ResourceTable, useMedplum } from '@medplum/react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { PatientHeader } from './PatientHeader';
 
 /**
  * This is an example of a generic "Resource Display" page.
  * It uses the Medplum `<ResourceTable>` component to display a resource.
  */
 export function ResourcePage(): JSX.Element | null {
-  const { resourceType, id } = useParams();
-  const resource = useResource({ reference: `${resourceType}/${id}` });
+  const medplum = useMedplum();
+  const { resourceType, id, versionId } = useParams();
+  const [resource, setResource] = useState<Resource | undefined>(undefined);
+
+  useEffect(() => {
+    if (resourceType && id) {
+      if (versionId) {
+        medplum.readVersion(resourceType as ResourceType, id, versionId).then(setResource);
+      } else {
+        medplum.readResource(resourceType as ResourceType, id).then(setResource);
+      }
+    }
+  }, [medplum, resourceType, id, versionId]);
+
   if (!resource) {
     return null;
   }
+
   return (
     <>
-      {resourceType === 'Patient' && <PatientHeader patient={resource as Patient} />}
-      <Document>
+      <Document key={[resourceType, id, versionId].join('-')}>
         <h2>{getDisplayString(resource)}</h2>
+        {versionId && <h3>Version: {versionId}</h3>}
         <div>
-          <ResourceTable key={`${resourceType}/${id}`} value={resource} ignoreMissingValues={true} />
+          <ResourceTable key={`${resourceType}/${id}`} value={resource} ignoreMissingValues={false} />
         </div>
       </Document>
     </>
